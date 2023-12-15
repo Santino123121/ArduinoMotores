387ba0f001704ad088c48546e78545e76bc4c13a
s <147212080+Santino123121@users.noreply.github.com>
2023-12-15 20:08:30 -0300

    Add files via upload

---
+++ ServoMotorControl/ServoMotorControl.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+
+// este programa trata de controlar la velocidad de un ServoMotor
+
+Servo myServo;
+void setup() {
+  myServo.attach(9);
+}
+
+void loop() {
+  giroizq(0, 90, 3);//se pone primero donde iniciara el Motor, donde terminara y la velocidad(recomiendo usar multiplos del numero en donde terminara el motor)
+  giroder(90, 0, 3);
+}
+void giroizq(int inicio,int fin,int speedy){
+  while(inicio<=fin){//se compara donde vaya a iniciar el ServoMotor y donde terminara
+    myServo.write(inicio); //si el ServoMotor no llego al fin asignado ira hacia donde la variable inicio este
+    delay(100);
+    inicio = inicio + speedy;// se suma la variable speed hasta que inicio sea igual a la variable fin y su programa termine
+    }
+}
+void giroder(int inicio,int fin,int speedy){
+  while(inicio>=fin){//se compara donde vaya a iniciar el ServoMotor y donde terminara
+    myServo.write(inicio); //si el ServoMotor no llego al fin asignado ira hacia donde la variable inicio este
+    delay(100);
+    inicio = inicio - speedy;// se resta la variable speed hasta que inicio sea igual a la variable fin y su programa termine
+    }
+}
